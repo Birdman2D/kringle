5e4fd8d00b2253e951b153268021405e72b3e126
rk <rk@rks-MacBook-Pro.local>
2018-12-22 14:14:16 +1300

    doorpasscode.py

---
+++ doorpasscode.py
@@ -0,0 +1,13 @@
+import requests
+from itertools import product
+import json
+
+digits = ['0','1','2','3']
+keyspace = [''.join(i) for i in list(product(digits, repeat=4))]
+rid = 'dcf77635-6971-4503-a8ea-87bcabf0c07c'
+for code in keyspace:
+    r = requests.get(f"{url}{code}&resourceId={rid}")
+    r = json.loads(r.text)
+    if r['success']:
+        print(code, r)
+        break;
