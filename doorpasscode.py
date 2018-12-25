eef99f3a01a456e806837da4ef04301170933576
r k <rk@rk.local>
2018-12-25 13:32:55 +1300

    testing book

--- doorpasscode.py
+++ doorpasscode.py
@@ -4,6 +4,7 @@ import json
 
 digits = ['0','1','2','3']
 keyspace = [''.join(i) for i in list(product(digits, repeat=4))]
+url ='https://doorpasscode.kringlecastle.com/checkpass.php?i='
 rid = 'dcf77635-6971-4503-a8ea-87bcabf0c07c'
 for code in keyspace:
     r = requests.get(f"{url}{code}&resourceId={rid}")
